7a9b41183e1c1e924d8c097adbfeb94f7473a4d2
Brietoe <Brietoe@gmail.com>
2021-04-18 04:30:07 -0500

    Freeze

--- Source/Testing.ts
+++ Source/Testing.ts
@@ -12,13 +12,11 @@ export default function Testing<FunctionUnderTest extends Mux>(FUT: FunctionUnde
 		debug(`[${i++}] Testing ${FUT.name} with ${inputs}`);
 		const output = FUT(...inputs);
 
-		const tester =
-		{
+		return Object.freeze({
 			is: Asserts(output),
 			theﾠresponseﾠmustﾠbeﾠa: Asserts(output),
 			claim: (testClaim: TestClaim<FunctionUnderTest>) => Asserts(testClaim(output)).truthy,
 			theﾠresult: (testClaim: TestClaim<FunctionUnderTest>) => Asserts(testClaim(output)).truthy
-		};
-		return tester;
+		});
 	};
 };;
