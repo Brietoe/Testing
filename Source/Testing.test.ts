51776c445e8066631978fb933890833667bf0144
Brietoe <Brietoe@gmail.com>
2021-04-18 03:51:57 -0500

    Claim

---
+++ Source/Testing.test.ts
@@ -0,0 +1,10 @@
+
+import Increment from "./Sample/Increment.js";
+import Testing from "./Testing.js";
+
+const increment = Testing(Increment);
+increment(10).is.equalTo(11);
+increment(10).is.not.not.equalTo(11);
+increment(10).is;
+increment(10).is.not.equalTo(-1);
+increment(10).is.not.equalTo(100);
---
+++ Source/Testing.ts
@@ -0,0 +1,30 @@
+
+import { Mux, Asserts } from "@cc-tech/library";
+import { debug } from "console";
+import { TestClaim } from "./TestClaim.js";
+
+export default function Testing<FunctionUnderTest extends Mux>(FUT: FunctionUnderTest)
+{
+	let i = 1;
+	debug("Testing", FUT.name, "function");
+	return (...inputs: Parameters<FunctionUnderTest>) =>
+	{
+		debug(`[${i++}] Testing ${FUT.name} with ${inputs}`);
+		const output = FUT(...inputs);
+
+		const tester =
+		{
+			is: Asserts(output),
+			theﾠresponseﾠmustﾠbeﾠa: Asserts(output),
+			claim: (testClaim: TestClaim<FunctionUnderTest>) =>
+			{
+				Asserts(testClaim(output)).truthy;
+			},
+			theﾠresult: (testClaim: TestClaim<FunctionUnderTest>) =>
+			{
+				Asserts(testClaim(output)).truthy;
+			}
+		};
+		return tester;
+	};
+};;
